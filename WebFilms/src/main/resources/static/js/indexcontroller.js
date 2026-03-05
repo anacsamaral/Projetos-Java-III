6353e9b34e86904756682bc63ba2f3cd83d858c0
Ana Amaral <anacarolinesousadoamaral2020@gmail.com>
2026-03-05 18:59:20 -0300

    upload de arquivos

--- WebFilms/src/main/resources/static/js/indexcontroller.js
+++ WebFilms/src/main/resources/static/js/indexcontroller.js
@@ -1,14 +1,16 @@
 function carregarFilmeAleatorio(){
-    //alert("Carregando filme aleatório");
-    const filmeSugerido = document.getElementById("filme-sugerido");
-    //filmeSugerido.innerHTML = "Titanic";
+   //alert("Carregando filme aleatório");
+   const filmeSugerido=document.getElementById("filme-sugerido");
+   fetch("http://localhost:8080/apis/random-movie")
+       .then(response => response.json()
+           .then(movie => {
+              filmeSugerido.innerHTML=`Assista ainda hoje o filme ${movie.title}, lançado em ${movie.year}`;
+       }))
+       .catch(error => {filmeSugerido.innerHTML=error;})
 
-    // fetch api, ajax, jquery, axios
-    fetch("http://localhost:8080/apis/random-movie")
-        .then(response => response.json().then(movie => {
-            filmeSugerido.innerHTML = `Assista ainda hoje o filme ${movie.title}, lançado em ${movie.year}`; // converte um objeto JSON em String -> ao contrário é JSON.parse
-        }))
-        .catch(error => {
-            filmeSugerido.innerHTML = error;
-        })
+   //filmeSugerido.innerHTML="Titanic";
+   //fetch api
+   //ajax
+   //jquery
+   //axios
 }
