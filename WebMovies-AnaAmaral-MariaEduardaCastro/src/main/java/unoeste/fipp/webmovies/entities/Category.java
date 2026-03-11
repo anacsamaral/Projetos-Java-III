38b41bc82f2df5d9e61d47c5f44f0da4e34a3e08
Ana Amaral <anacarolinesousadoamaral2020@gmail.com>
2026-03-11 18:41:42 -0300

    Web Movies

---
+++ WebMovies-AnaAmaral-MariaEduardaCastro/src/main/java/unoeste/fipp/webmovies/entities/Category.java
@@ -0,0 +1,5 @@
+package unoeste.fipp.webmovies.entities;
+
+public record Category(String id, String nome) {
+
+}
---
+++ WebMovies-AnaAmaral-MariaEduardaCastro/src/main/resources/static/js/indexcontroller.js
@@ -0,0 +1,10 @@
+function carregarFilmeAleatorio(){
+   //alert("Carregando filme aleatório");
+   const filmeSugerido=document.getElementById("filme-sugerido");
+   fetch("http://localhost:8080/apis/random-movie")
+       .then(response => response.json()
+           .then(movie => {
+              filmeSugerido.innerHTML=`Assista ainda hoje o filme ${movie.title}, lançado em ${movie.year}`;
+       }))
+       .catch(error => {filmeSugerido.innerHTML=error;})
+}
